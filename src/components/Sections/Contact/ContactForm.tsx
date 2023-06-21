23f9ef093e908e5a0c56b00410cd8868538b9340
Bibin Alias <bibinalias1@gmail.com>
2023-06-21 20:01:11 +0530

    add chimeny project edited experiance as paragraph, add ahobby and contact updated

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,4 +1,6 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
+import { FC, memo, useCallback, useMemo, useState } from 'react';
+
+import axios from 'axios';
 
 interface FormData {
   name: string;
@@ -20,25 +22,61 @@ const ContactForm: FC = memo(() => {
 
   const onChange = useCallback(
     <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
+      const { name, value } = event.target;
 
-      const fieldData: Partial<FormData> = {[name]: value};
+      const fieldData: Partial<FormData> = { [name]: value };
 
-      setData({...data, ...fieldData});
+      setData({ ...data, ...fieldData });
     },
     [data],
   );
+
+  // const handleSendMessage = useCallback(
+  //   async (event: React.FormEvent<HTMLFormElement>) => {
+  //     event.preventDefault();
+  //     /**
+  //      * This is a good starting point to wire up your form submission logic
+  //      * */
+  //     console.log('Data to send: ', data);
+  //   },
+  //   [data],
+  // );
+
 
   const handleSendMessage = useCallback(
     async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      /**
-       * This is a good starting point to wire up your form submission logic
-       * */
+
+
+      const form = new FormData();
+      form.append("from", data.name + " " + data.email);
+      form.append("to", "Bibin Alias bibinalias1@gmail.com");
+      form.append("subject", "Resume Website Message");
+      form.append("text", data.message);
+
+      try {
+        await axios.post(
+          "https://api.mailgun.net/v3/sandbox674cf946b913433881426bccdcc2fe1c.mailgun.org/messages",
+          form,
+          {
+            auth: {
+              username: 'api',
+              password: 'bd2c58482b1693ac4580a44495d3b12b-135a8d32-cad8c23b',
+            }, headers: { 'Content-Type': 'multipart/form-data' },
+          }
+        );
+
+        console.log('Email sent successfully!');
+        // console.log(response.data);
+      } catch (error) {
+        console.error('Error sending email:', error);
+      }
       console.log('Data to send: ', data);
+      //setData({...data, ...defaultData});
     },
     [data],
   );
+
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
@@ -64,12 +102,12 @@ const ContactForm: FC = memo(() => {
         required
         rows={6}
       />
-      {/* <button
+      {<button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
         type="submit">
         Send Message
-      </button> */}
+      </button>}
     </form>
   );
 });

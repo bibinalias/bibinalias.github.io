522073d58af0230a7f00e17c7c7e8574e48d35d3
Bibin Alias <bibinalias1@gmail.com>
2026-08-15 20:50:35 +0530

    seo: fix canonical/OG to bibinalias.github.io, add sitemap, robots, and Google verification

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -5,8 +5,12 @@ import {memo} from 'react';
 
 import {HomepageMeta} from '../../data/dataDef';
 
+const SITE_URL = 'https://bibinalias.github.io';
+
 const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
   const {asPath: pathname} = useRouter();
+  const canonicalUrl = `${SITE_URL}${pathname}`;
+  const ogImageUrl = `${SITE_URL}/1849594.png`;
 
   return (
     <>
@@ -15,7 +19,7 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <meta content={description} name="description" />
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
-        <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
+        <link href={canonicalUrl} key="canonical" rel="canonical" />
 
         <link href="/1849565.png" rel="icon" sizes="any" />
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
@@ -23,13 +27,17 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <link href="/site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
+        <meta content="website" property="og:type" />
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
-        <meta content={`https://reactresume.com${pathname}`} property="og:url" />
+        <meta content={canonicalUrl} property="og:url" />
+        <meta content={ogImageUrl} property="og:image" />
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
+        <meta content="summary_large_image" name="twitter:card" />
         <meta content={title} name="twitter:title" />
         <meta content={description} name="twitter:description" />
+        <meta content={ogImageUrl} name="twitter:image" />
       </Head>
       {children}
     </>
